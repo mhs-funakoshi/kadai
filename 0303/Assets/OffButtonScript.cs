a9c8142dfb5f6dac783147adbe03cece72514968
mhs-funakoshi <funakoshi_taku@mhsolutions.co.jp>
2020-03-03 16:00:01 +0900

    first

---
+++ 0303/Assets/OffButtonScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OffButtonScript : MonoBehaviour
+{
+    Text text1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text1 = GameObject.Find("Text").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        text1.enabled = false;
+    }
+}
